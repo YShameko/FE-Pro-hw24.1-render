e16227811c9a0a37605bdf4f44754cdc1dbbb09f
YShameko <yshameko@gmail.com>
2025-09-07 16:04:29 +0300

    +mongo db

--- server/index.mjs
+++ server/index.mjs
@@ -2,9 +2,22 @@ import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "path";
 import url from "url";
+import mongoose from "mongoose";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/db1';
+
+console.log('MongoDB path is: ', mongoUrl);
+mongoose.connect(mongoUrl);
+
+mongoose.connection.on('open', () => {
+    console.log('MongoDB is connected successfully');
+});
+
+mongoose.connection.on('error', () => {
+    console.log('MongoDB is failed to connect');
+});
 
 const app = fastify();
 
